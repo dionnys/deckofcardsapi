910c6d3192942c8da711fb1c76e478263b5d98fa
Dionnys Bonalde <development@bitexperts.cl>
2021-08-14 20:53:36 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import requests
+
+if __name__ == '__main__':
+    url = 'https://deckofcardsapi.com/api/deck/new/shuffle/'
+    args = {'desk_count': '1'} 
+    response = requests.get(url, params=args)
+
+    
+    print (response)
+    print (response.url)
+    
+    if response.status_code == 200:
+        print (response.content)
+        content = response.content
+        
+        file = open ('archivo.txt', 'wb')
+        file.write(content)
+        file.close()
